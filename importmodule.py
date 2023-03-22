fb6f5b0c696f51c97f3f6a4fa2355ff736515827
Hunter Brooks <85brookshunter85@gmail.com>
2023-03-22 15:17:15 -0700

    new file:   importmodule.py

---
+++ importmodule.py
@@ -0,0 +1,14 @@
+# Import all needed packages.
+# ------------------------------------------------------------- #
+import csv
+import math
+import time
+import sys,os
+import astropy
+import numpy as np
+import pandas as pd
+import PySimpleGUI as sg
+# ------------------------------------------------------------- #
+#Ignores all of the warnings from the packages above
+import warnings
+warnings.filterwarnings("ignore")
