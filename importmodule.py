bbd5e5ccdd89b8f804bde7ac2d71659b043f1da1
AstroNumNums <128646883+AstroNumNums@users.noreply.github.com>
2023-03-22 15:35:24 -0700

    new file:   importmodule.py modified:   README.txt

---
+++ importmodule.py
@@ -0,0 +1,13 @@
+# Import all needed packages.
+# ------------------------------------------------------------- #
+import csv
+import math
+import time
+import astropy
+import numpy as np
+import pandas as pd
+import PySimpleGUI as sg
+# ------------------------------------------------------------- #
+#Ignores all of the warnings from the packages above
+import warnings
+warnings.filterwarnings("ignore")
