7d068d9d68a92a07dc31848ba18e4606d579578f
niteflyunicorns <savannahj1200@gmail.com>
2023-04-12 16:32:04 -0700

    changed math import

--- solarinsolation10.py
+++ solarinsolation10.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
-import math
+#import math
+from math import sin, cos, tan, atan
 
 # Constants and parameters
 a_s = 9.539  # AU
